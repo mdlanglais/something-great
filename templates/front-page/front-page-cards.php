c86ccab711f3942257acce7428b3344333a0e608
Michael Langlais <mdlanglais@me.com>
2018-05-09 18:26:29 -0500

    Update insecure http links to dummyimage.com to https to ensure the theme will not produce mixed content warnings in the browser.

--- templates/front-page/front-page-cards.php
+++ templates/front-page/front-page-cards.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col-md-4">
         <div class="card">
-            <img class="card-img-top" src="http://dummyimage.com/600x400/222222/eeeeee" alt="Card image cap">
+            <img class="card-img-top" src="https://dummyimage.com/600x400/222222/eeeeee" alt="Card image cap">
             <div class="card-body">
                 <h4 class="card-title">Card title</h4>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -11,7 +11,7 @@
     </div>
     <div class="col-md-4">
         <div class="card">
-            <img class="card-img-top" src="http://dummyimage.com/600x400/222222/eeeeee" alt="Card image cap">
+            <img class="card-img-top" src="https://dummyimage.com/600x400/222222/eeeeee" alt="Card image cap">
             <div class="card-body">
                 <h4 class="card-title">Card title</h4>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -21,7 +21,7 @@
     </div>
     <div class="col-md-4">
         <div class="card">
-            <img class="card-img-top" src="http://dummyimage.com/600x400/222222/eeeeee" alt="Card image cap">
+            <img class="card-img-top" src="https://dummyimage.com/600x400/222222/eeeeee" alt="Card image cap">
             <div class="card-body">
                 <h4 class="card-title">Card title</h4>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
